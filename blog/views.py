ef89c14f224a28fe64d5313138a0c4b46e8091c5
HellMike <vasinm455@gmail.com>
2022-10-24 01:30:43 +0300

    Changed date format, import models to admin

--- blog/views.py
+++ blog/views.py
@@ -1,24 +1,10 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author': 'SemenEV',
-        'title': 'Post 1',
-        'content': 'First post content',
-        'date_posted': 'March 15, 2007'
-    },
-    {
-        'author': 'VladimirKS',
-        'title': 'Post 6',
-        'content': 'Sixth post content',
-        'date_posted': 'July 25, 2009'
-    }
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
